a4a055ad77ffd38a2c7e0d9f9c94d9f1fc20c5cb
Simone Russchen <simone.russchen@polteq.com>
2017-12-28 09:22:45 +0100

    correct ignore and add test

---
+++ cs/BootCamp/BootCamp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BootCamp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BootCamp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cad6ed6d-78d9-4d39-93d0-aa06f8a1163d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ cs/BootCamp/FirstTestInloggen.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BootCamp
+{
+    [TestClass]
+    public class FirstTestInloggen
+    {
+        [TestMethod]
+        public LogInSuccessFull()
+        {
+            //waar zie ik deze file?
+        }
+    }
+}
